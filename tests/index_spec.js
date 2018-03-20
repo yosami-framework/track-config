2f368024627d95458ed66387163bfc2f8fa202c2
alfa <alfa.jpn@gmail.com>
2018-03-20 21:36:56 +0900

    Fix setting item.

--- tests/index_spec.js
+++ tests/index_spec.js
@@ -2,31 +2,6 @@ const t           = require('track-spec');
 const TrackConfig = require('../lib/index');
 
 t.describe('TrackConfig', () => {
-  t.describe('#m', () => {
-    const subject = (() => TrackConfig.m);
-
-    t.beforeEach(() => {
-      global.TrackConfigs.m = 'hoge';
-    });
-
-    t.it('Get .m', () => {
-      t.expect(subject()).equals('hoge');
-    });
-  });
-
-  t.describe('#m=', () => {
-    const subject = (() => {
-      TrackConfig.configure((c) => {
-        c.m = 'fuga';
-      });
-    });
-
-    t.it('Set .m', () => {
-      subject();
-      t.expect(global.TrackConfigs.m).equals('fuga');
-    });
-  });
-
   t.describe('#loader', () => {
     const subject = (() => TrackConfig.loader);
 
